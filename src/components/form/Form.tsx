50bb12d32cb85c5e5edf0b495205b8c78c8b1379
nestorjavier <nestor06sep@gmail.com>
2024-01-23 22:23:41 -0400

    init form

---
+++ src/components/form/Form.tsx
@@ -0,0 +1,51 @@
+/*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+
+"use client";
+
+export default function Form() {
+  return (
+    <div className="">
+      <div className="mx-auto max-w-2xl px-4 pb-24 pt-16 sm:px-6 lg:max-w-7xl lg:px-8">
+        <form className="lg:grid lg:grid-cols-2 lg:gap-x-12 xl:gap-x-16">
+          <div>
+            <h2 className="text-lg font-medium">Título del formulario</h2>
+
+            <div className="mt-4 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-4">
+              <div>
+                <label
+                  htmlFor="first-name"
+                  className="block text-sm font-medium"
+                >
+                  First name
+                </label>
+                <div className="mt-1">
+                  <input
+                    type="text"
+                    id="first-name"
+                    name="first-name"
+                    autoComplete="given-name"
+                    className="block w-full  rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
