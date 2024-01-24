a6b47b40deeb6356b97939200815377692f3b42c
Jose Perez <16-10882@usb.ve>
2024-01-24 18:45:21 -0400

    anadiendo campos faltantes al form

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -20,7 +20,7 @@ const response = {
   error: null,
   data: {
     id: "unique_form_identifier",
-    title: "titulo del formulario",
+    title: "Formulario numero 1",
     questions: [
       {
         id: "q1",
@@ -59,17 +59,14 @@ export default function Form() {
       <div className="mx-auto max-w-2xl px-4 pb-24 pt-16 sm:px-6 lg:max-w-7xl lg:px-8">
         <form className="lg:grid lg:grid-cols-2 lg:gap-x-12 xl:gap-x-16">
           <div>
-            <h2 className="text-lg font-medium">Título del formulario</h2>
+            <h2 className="text-lg font-medium">{response.data.title}</h2>
 
             <div className="mt-4 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-4">
               {response.data.questions.map((field: any, index: number) => {
                 if (field.type === "short-text" || field.type === "long-text") {
                   return (
                     <div key={index}>
-                      <label
-                        htmlFor={field.id}
-                        className="block text-sm font-medium"
-                      >
+                      <label htmlFor={field.id} className="block text-sm font-medium">
                         {field.label}
                       </label>
                       <div className="mt-1">
@@ -78,7 +75,7 @@ export default function Form() {
                           id={field.id}
                           name={field.id}
                           autoComplete="given-name"
-                          className="block w-full  rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                          className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                         />
                       </div>
                     </div>
@@ -86,10 +83,7 @@ export default function Form() {
                 } else if (field.type === "single-choice") {
                   return (
                     <div key={index} className="">
-                      <label
-                        htmlFor=""
-                        className="block text-sm font-medium"
-                      >
+                      <label htmlFor="" className="block text-sm font-medium">
                         {field.label}
                       </label>
                       <div className="mt-1">
@@ -108,6 +102,54 @@ export default function Form() {
                             );
                           })}
                         </select>
+                      </div>
+                    </div>
+                  );
+                } else if (field.type === "multiple-choice") {
+                  return (
+                    <div key={index} className="">
+                      <label htmlFor="" className="block text-sm font-medium">
+                        {field.label}
+                      </label>
+                      <div className="mt-1">
+                        {field.options.map((option: any, index: number) => {
+                          return (
+                            <div key={index} className="flex items-start">
+                              <div className="flex items-center h-5">
+                                <input
+                                  id={`${field.id}-${index}`}
+                                  name={field.id}
+                                  type="checkbox"
+                                  className="focus:ring-myblue h-4 w-4 text-myblue border-gray-300 rounded"
+                                />
+                              </div>
+                              <div className="ml-3 text-sm">
+                                <label
+                                  htmlFor={`${field.id}-${index}`}
+                                  className="font-medium text-gray-700"
+                                >
+                                  {option}
+                                </label>
+                              </div>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  );
+                } else if (field.type === "file-upload") {
+                  return (
+                    <div key={index}>
+                      <label htmlFor={field.id} className="block text-sm font-medium">
+                        {field.label}
+                      </label>
+                      <div className="mt-1">
+                        <input
+                          type="file"
+                          id={field.id}
+                          name={field.id}
+                          className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                        />
                       </div>
                     </div>
                   );
