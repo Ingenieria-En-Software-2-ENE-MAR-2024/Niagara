856096fa71b541ffb4fbee57418e2a9ff811ade2
Daniela <randrade.daniela@gmail.com>
2024-01-23 23:17:42 -0400

    feat(login): added login's structure

---
+++ src/app/pages/Login.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "../components/login";
+
+const LoginPage = () => {
+  return (
+    <div>
+        {/* header */}
+        <Login />
+         {/* footer */}
+    </div>
+  );
+};
+
+export default LoginPage;
