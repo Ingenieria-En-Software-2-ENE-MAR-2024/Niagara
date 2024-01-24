c25ce05b662c3abcd4142a309616824befa69049
Nestor <16-10455@gmail.com>
2024-01-24 11:37:01 -0400

    added conditional rendering fields

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -15,6 +15,44 @@
 
 "use client";
 
+const response = {
+  loading: false,
+  error: null,
+  data: {
+    id: "unique_form_identifier",
+    title: "titulo del formulario",
+    questions: [
+      {
+        id: "q1",
+        type: "single-choice",
+        label: "Pregunta de Selección Simple",
+        options: ["Opción 1", "Opción 2", "Opción 3"],
+      },
+      {
+        id: "q2",
+        type: "multiple-choice",
+        label: "Pregunta de Selección Múltiple",
+        options: ["Opción A", "Opción B", "Opción C"],
+      },
+      {
+        id: "q3",
+        type: "file-upload",
+        label: "Carga de Archivos",
+      },
+      {
+        id: "q4",
+        type: "short-text",
+        label: "Área de Texto Corto",
+      },
+      {
+        id: "q5",
+        type: "long-text",
+        label: "Área de Texto Largo",
+      },
+    ],
+  },
+};
+
 export default function Form() {
   return (
     <div className="">
@@ -24,25 +62,58 @@ export default function Form() {
             <h2 className="text-lg font-medium">Título del formulario</h2>
 
             <div className="mt-4 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-4">
-              <div>
-                <label
-                  htmlFor="first-name"
-                  className="block text-sm font-medium"
-                >
-                  First name
-                </label>
-                <div className="mt-1">
-                  <input
-                    type="text"
-                    id="first-name"
-                    name="first-name"
-                    autoComplete="given-name"
-                    className="block w-full  rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                  />
-                </div>
-              </div>
+              {response.data.questions.map((field: any, index: number) => {
+                if (field.type === "short-text" || field.type === "long-text") {
+                  return (
+                    <div key={index}>
+                      <label
+                        htmlFor={field.id}
+                        className="block text-sm font-medium"
+                      >
+                        {field.label}
+                      </label>
+                      <div className="mt-1">
+                        <input
+                          type="text"
+                          id={field.id}
+                          name={field.id}
+                          autoComplete="given-name"
+                          className="block w-full  rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                        />
+                      </div>
+                    </div>
+                  );
+                } else if (field.type === "single-choice") {
+                  return (
+                    <div key={index} className="">
+                      <label
+                        htmlFor=""
+                        className="block text-sm font-medium"
+                      >
+                        {field.label}
+                      </label>
+                      <div className="mt-1">
+                        <select
+                          defaultValue=""
+                          className="block w-full rounded-md border-gray-300 shadow-sm focus:border-myblue focus:ring-myblue sm:text-sm"
+                        >
+                          <option value="" disabled>
+                            Seleccione la entrada
+                          </option>
+                          {field.options.map((option: any, index: number) => {
+                            return (
+                              <option key={index} value={option}>
+                                {option}
+                              </option>
+                            );
+                          })}
+                        </select>
+                      </div>
+                    </div>
+                  );
+                }
+              })}
             </div>
-
           </div>
         </form>
       </div>
