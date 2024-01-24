49b6063b48ec96769e34cfc52ba10e0b0b35a3c9
Ana Santos <17-10602@usb.ve>
2024-01-24 01:28:52 -0400

    form login sencillo

--- src/app/components/login.tsx
+++ src/app/components/login.tsx
@@ -3,7 +3,14 @@ import React from "react";
 const Login = () => {
   return (
     <div>
-      <h1>Login</h1>
+      <h1>Niagara</h1>
+      <form className='flex flex-col items-center'>
+        <input className='form-element' id='userName' type='string' placeholder='Enter your user' />
+        <input className='form-element' id='password' type='password' placeholder='Enter your password'/>
+        
+        <button className='form-buttom' type='submit'> Sign In</button>
+        
+      </form>
     </div>
   );
 };
