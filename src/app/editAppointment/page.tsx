c119edb1334377dee71428f0345475203b07d50d
Simon Puyosa <18-10717@usb.ve>
2024-02-26 20:16:45 -0400

    ADDED: editAppointment

---
+++ src/app/editAppointment/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import Link from 'next/link';
+import { FormEditAppointment } from "@/components/FormEditAppointment";
+
+
+export default function EditAppointment() {
+  return (
+
+    <FormEditAppointment />
+
+  )
+}
---
+++ src/components/FormEditAppointment.tsx
@@ -0,0 +1,123 @@
+'use client'
+
+import React, { useState } from 'react'
+import { TextField, SelectField } from '@/components/Fields'
+import { AuthLayout } from '@/components/AuthLayout';
+import { Button } from '@/components/Button';
+import { useRouter } from 'next/router';
+
+
+interface Props {
+    idPacient: string;
+    name: string;
+    speciality: string;
+    doctor: string;
+    originalDate: string;
+    originalTime: string;
+    originalDescription: string;
+}
+
+export function FormEditAppointment({ idPacient = "32", name = "Adolf Hitler", speciality = "Ginecologia", doctor = "Hugo Chavez", originalDate = "22/03/2023", originalTime = "10:00", originalDescription = "Necesita un cambio de genero" }: Props) {
+    const [date, setDate] = useState(originalDate);
+    const [time, setTime] = useState(originalTime);
+    const [description, setDescription] = useState(originalDescription);
+    const [changeReason, setChangeReason] = useState('');
+    const [errorMessage, setErrorMessage] = useState(''); 
+
+    const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        console.log({ date, time, description }); // aqui iria el fetch
+    }
+
+    const onCancel = () => {
+        const router = useRouter();
+        router.back();
+    }
+
+    const generateTimeOptions = () => {
+        const options = [];
+        for (let hour = 8; hour <= 17; hour++) {
+            const timeString = `${hour}:00`;
+            options.push(
+                <option key={timeString} value={timeString}>{timeString}</option>
+            );
+        }
+        return options;
+    }
+
+
+    return (
+        <AuthLayout 
+        title="Editción de la cita médica"
+        subtitle=""
+        >
+            {errorMessage && <p style={{color: 'red'}}>{errorMessage}</p>}
+            <form onSubmit={onSubmit}>
+                <TextField
+                    value={idPacient}
+                    label="ID Paciente"
+                    disabled
+                    className="mt-6"
+                />
+
+                <TextField
+                    value={name}
+                    label="Nombre y Apellido"
+                    disabled
+                    className="mt-6"
+                />
+
+                <TextField
+                    value={speciality}
+                    label="Área o Especialidad"
+                    disabled
+                    className="mt-6"
+                />
+
+                <TextField
+                    value={doctor}
+                    label="Médico o Especialista"
+                    disabled
+                    className="mt-6"
+                />
+
+                <TextField
+                    value={date}
+                    onChange={(e) => setDate(e.target.value)}
+                    label="Fecha (dd/mm/yy)"
+                    className="mt-6"
+                />
+
+                <SelectField
+                    value={time}
+                    onChange={(e) => setTime(e.target.value)}
+                    label="Hora"
+                    className="mt-6"
+                >
+                    {generateTimeOptions()}
+                </SelectField>
+
+                <TextField
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    label="Descripción"
+                    className="mt-6"
+                />
+
+                <TextField
+                    value={changeReason}
+                    onChange={(e) => setChangeReason(e.target.value)}
+                    label="Razón de cambio"
+                    className="mt-6"
+                />
+
+                <div style={{ marginTop: '20px', display: 'flex', justifyContent: 'space-between', maxWidth: '200px', margin: '20px auto' }}>
+                    <Button variant='solid' color="cyan" type='submit'> Confirmar </Button>
+                    <Button variant='outline' color="gray" onClick={onCancel}> Cancelar </Button>
+                </div>
+
+            </form>
+        </AuthLayout>
+    );
+};
