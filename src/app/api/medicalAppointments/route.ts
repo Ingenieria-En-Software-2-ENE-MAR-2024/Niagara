e7546b602fa0cc45a00644e3bac40c46df09b6f4
JKOMI2303 <16-10109@usb.ve>
2024-02-27 21:51:11 -0400

    Algo del CRUD de citas (cableado)

--- src/app/api/medicalAppointments/route.ts
+++ src/app/api/medicalAppointments/route.ts
@@ -1,11 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { post_medical_appointment } from '@/app/controllers/medical_appointment'
+import { get_medical_appointments, post_medical_appointment } from '@/app/controllers/medical_appointment'
 
 export async function POST(req: NextRequest) {
     try {
-      // console.log('LLego al route de post user')
-      const new_user = await post_medical_appointment (req)
-      return NextResponse.json(new_user, { status: 201 })
+      const new_medical_appointment = await post_medical_appointment (req)
+      return NextResponse.json(new_medical_appointment, { status: 201 })
     } catch (err: any) {
       const error_json = {
         error_message: err.error_message,
@@ -15,3 +14,19 @@ export async function POST(req: NextRequest) {
       return NextResponse.json(error_json, { status: err.status })
     }
   }
+
+
+  export async function GET(req: NextRequest) {
+    try {
+      const list_medical_appointments = await get_medical_appointments(req)
+      return NextResponse.json(list_medical_appointments, { status: 200 })
+    } catch (err: any) {
+      const error_json = {
+        error_message: err.error_message,
+        error_message_detail: err.error_message_detail,
+        error_code: err.error_code,
+      }
+      return NextResponse.json(error_json, { status: err.status })
+    }
+  }
+  
