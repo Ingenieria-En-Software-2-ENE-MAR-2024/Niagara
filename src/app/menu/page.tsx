75d21ee5abb0c68c003ef540fbb592bb9d9f1a9f
Giancarlo Dente <gian20.04@hotmail.com>
2024-02-28 23:09:18 -0400

    menu ahora admite submenus

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -3,18 +3,31 @@ import React from 'react';
 import NavigationMenu from '../../components/NavigationMenu';
 
 const Menu: React.FC = () => {
+    
     const menuItems = [
-        {title: 'Inicio', link: '/',},
-        {title: 'Cambio de contraseña', link: '/',},
+        { title: 'Inicio', link: '/' },
+        { 
+            title: 'Configuración', 
+            subMenuItems: [
+            { title: 'Gestión de Usuarios', link: '/' },
+            { title: 'Logger de Eventos', link: '/logger' },
+            {
+                title: 'Cambio de password',
+                subMenuItems: [
+                { title: 'Prueba', link: '/' },
+                ]
+            },
+            ]
+        },
         {
-        title: 'Configuración',
+        title: 'Servicios',
         subMenuItems: [
-            { title: 'Roles de Usuarios', link: '/' },
-            { title: 'Logger de Eventos', link: '/' },
+            { title: 'Servicio1', link: '/' },
+            { title: 'Servicio2', link: '/' },
         ],
         },
-        {title: 'Gestión de citas', link: '/',},
-        {title: 'Calendario médico', link: '/',},
+        { title: 'Gestión de Citas', link: '/' },
+        { title: 'Calendario Médico', link: '/' },
     ];
 
     return (
