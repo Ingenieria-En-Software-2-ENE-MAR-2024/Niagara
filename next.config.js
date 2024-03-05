461528f2584e60fd5465e367d67f0948bce32fd0
Boondock19 <josematiasgonzalez20@gmail.com>
2024-03-04 22:13:09 -0400

    Agregado cors al proyecto

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                // Match all routes
+                source:"/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
+                    { key: "Access-Control-Allow-Headers", value: "X-Requested-With, Content-Type, Accept, Origin" },
+                    { key: "Access-Control-Allow-Credentials", value: "true" }
+                ]
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
