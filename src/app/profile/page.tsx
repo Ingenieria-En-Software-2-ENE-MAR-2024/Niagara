d0e689f64a0a784da7ddb5cc7b6bc5de6f3a68d4
root <17-10602@gmail.com>
2024-03-19 18:33:47 -0400

    ADD: Profile page and Pacient Profile component

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { PacientProfile } from '@/components/profiles/Pacient';
+import Menu from '@/components/Menu';
+
+export default function Profile() {
+    /*
+    const [name, setName] = useState('');
+    const [username, setUsername] = useState('');
+    //const [id, setId] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [role, setRole] = useState('');
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('http://localhost:300/api/users', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            });
+            const data = await response.json();
+
+            setName(data.name);
+            setUsername(data.username);
+            //setId(data.ci);
+            setEmail(data.email);
+            setPassword(data.password);
+            setRole(data.role);
+        };
+
+        fetchData();
+    }, []);
+    */
+    const name = 'prueba';
+    const username = '@prueba';
+    const email = 'prueba@email.com';
+
+    return (
+        <div>
+            <Menu/>
+            
+            {//role === 'Pacient' ? (
+                <PacientProfile user={{ name, username, email }}/>
+            /*) : role === 'Medic' ? (
+                <h1>¡Hola doctor!</h1>
+            ) : null*/}
+            
+        </div>
+    )
+}
---
+++ src/components/profiles/Pacient.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { TextField } from '@/components/Fields';
+import { AuthLayout } from '@/components/AuthLayout';
+import { Button } from '@/components/Button';
+import { Typography } from '@mui/material';
+
+
+type PacientProps = {
+    user: {
+        name: string;
+        username: string;
+        //id: string;
+        email: string;
+    }
+}
+
+export const PacientProfile:React.FC<PacientProps> = ({user}) => {
+    return (
+        <AuthLayout
+        title = 'Perfil de Paciente'
+        subtitle = {`¡Hola ${user.name}!`}>
+            <div>
+                <Typography variant="body1" component="h1" style={{ fontWeight: 'bold' }}>
+                    Nombre:
+                </Typography>
+                <Typography variant="body2" component="h2" style={{ marginLeft: '30px', marginBottom :'32px', fontStyle: 'italic' }} gutterBottom>
+                    {user.name}
+                </Typography>
+                <Typography variant="body1" component="h1" style={{ fontWeight: 'bold' }}>
+                    Usuario:
+                </Typography>
+                <Typography variant="body2" component="h2" style={{ marginLeft: '20px', marginBottom :'32px', fontStyle: 'italic' }} gutterBottom>
+                    {user.username}
+                </Typography>
+                <Typography variant="body1" component="h1" style={{ fontWeight: 'bold' }}>
+                    Cédula de Identidad:
+                </Typography>
+                <Typography variant="body2" component="h2" style={{ marginLeft: '20px', marginBottom :'32px', fontStyle: 'italic' }} gutterBottom>
+                    12.345.678
+                </Typography>
+                <Typography variant="body1" component="h1" style={{ fontWeight: 'bold' }}>
+                    Correo electrónico:
+                </Typography>
+                <Typography variant="body2" component="h2" style={{ marginLeft: '20px', marginBottom :'32px', fontStyle: 'italic' }} gutterBottom>
+                    {user.email}
+                </Typography>
+            </div>
+            
+        </AuthLayout>
+    )
+}
