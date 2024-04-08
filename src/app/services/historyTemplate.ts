8a04342a910ed6ac033cb548c52c90a856311740
Jose Perez <16-10882@usb.ve>
2024-04-08 14:23:30 -0400

    complete get historyTemplate admin

--- src/app/services/historyTemplate.ts
+++ src/app/services/historyTemplate.ts
@@ -44,53 +44,14 @@ const create_history_template = async (admin_id:number,body: Thistory_template_c
 
   const obtain_all_history_template = async () => {
     try {
-      // const found_admin = await prisma.admin.findFirst()
+      const found_admin = await prisma.admin.findFirst()
   
-      // if (!found_admin) {
-      //   throw new Error('Admin does not exists')
-      // }
-  
-      // const all_history_template = await prisma.questionary.findMany()
-  
-      // if (!all_history_template) {
-      //   throw new Error('Error getting history template')
-      // }
-  
-      const all_history_template = {
-        id: 12,
-        admin_id: 2,
-        questionsType: [
-          {
-            "type": "TEXT",
-            "question": "Cual es tu nombre",
-             "section":["Datos personales"]
-           },
-          {
-           "type": "SIMPLE_SELECT",
-            "question": "Cual es su genero",
-           "options": ["Masculino","Femenino"],
-           "section":["Datos personales"]
-          },
-        {
-           "type": "MULTIPLE_SELECT",
-           "question": "Cuantas enfermedades tiene",
-           "options": ["Opcion1","Opcion2","Opcion3"],
-           "section":["Datos personales","Paciente"]
-          },
-        {
-           "type": "DATE",
-           "question": "Fecha de nacimiento",
-           "section":["Datos personales","Paciente"]
-          },
-          {
-           "type": "NUMBER",
-           "question": "Cual es tu edad",
-            "section":["Datos personales"]
-          }
-        ],
-        createdAt: '2024-04-03T17:53:00.417Z',
-        updatedAt: '2024-04-03T17:53:00.417Z',
+      if (!found_admin) {
+        throw new Error('Admin does not exists')
       }
+  
+      const all_history_template = await prisma.questionary.findMany()
+      
       return all_history_template
     } catch (error) {
       throw error
